6666375f2c249118b084186ab9475aa786016c56
Jimmy Ngo <70559170+JimmyJamXD@users.noreply.github.com>
2023-06-05 20:10:50 -0700

    Add files via upload

---
+++ Homework 06/starter_code/js/trulia.js
@@ -0,0 +1,37 @@
+
+
+let truliaCards = document.getElementsByClassName("trulia-grid-item");
+
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+// Toggle the navigation
+document.querySelector(".trulia-nav-toggle").addEventListener('click',function(e){
+  e.preventDefault();
+  let toggle = document.querySelectorAll('.trulia-nav > nav ul');
+  for(let i=0; i<toggle.length; i++){
+    toggle[i].classList.toggle('trulia-nav-mobilehide');
+  }
+
+});
+
+
+// Loop through all the cards
+let grid_item = document.querySelectorAll(".trulia-grid-item");
+for(let i = 0; i < grid_item.length; i++){
+  //Add a click listener on each card
+  grid_item[i].addEventListener('click', function(e){
+      // Remove the featured class
+      for(let j = 0; j <grid_item.length; j++){
+        grid_item[j].classList.remove('trulia-featured-grid-item');
+      }
+      // Add the featured class on the one clicked on
+      this.classList.add('trulia-featured-grid-item');
+  });
+  // console.log(grid_item[i]);
+}
+  });  
+
+
+
